bd072e9cdbf0022f8c4cee27a716358504947268
sato kyohei <>
2025-03-25 10:37:14 +0900

    buildbuild

--- pacman_web/lib/about.dart
+++ pacman_web/lib/about.dart
@@ -18,50 +18,52 @@ class HomeState extends State<About> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        width: double.infinity,
-        height: 3000,
-        color: Colors.white,
-        child: Column(children: [
-          Container(
-              width: double.infinity,
-              constraints: const BoxConstraints(minHeight: 100),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 0, // 影の広がり
-                    blurRadius: 10, // ぼかし範囲
-                    offset: const Offset(0, 1), // 下方向に影を移動
-                  ),
-                ],
-              ),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                SizedBox(
-                  height: 40,
-                  width: 200,
-                  child: Image.asset('assets/logo.png'),
+    return Material(
+      child: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: 3000,
+          color: Colors.white,
+          child: Column(children: [
+            Container(
+                width: double.infinity,
+                constraints: const BoxConstraints(minHeight: 100),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 0, // 影の広がり
+                      blurRadius: 10, // ぼかし範囲
+                      offset: const Offset(0, 1), // 下方向に影を移動
+                    ),
+                  ],
                 ),
-                const Spacer(),
-                Expanded(
-                  child: Wrap(
-                    children: [
-                      button("ABOUT", () {
-                        context.push('/about');
-                      }),
-                      button("BUSINESS", () {}),
-                      button("PRODUCT", () {}),
-                      button("WORKS", () {}),
-                      button("COMPANY", () {}),
-                      button("RECRUIT", () {}),
-                    ],
+                child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                  SizedBox(
+                    height: 40,
+                    width: 200,
+                    child: Image.asset('assets/logo.png'),
                   ),
-                ),
-              ])),
-          Text("私たちの強み", style: GoogleFonts.notoSans(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.black)),
-        ]),
+                  const Spacer(),
+                  Expanded(
+                    child: Wrap(
+                      children: [
+                        button("ABOUT", () {
+                          context.push('/about');
+                        }),
+                        button("BUSINESS", () {}),
+                        button("PRODUCT", () {}),
+                        button("WORKS", () {}),
+                        button("COMPANY", () {}),
+                        button("RECRUIT", () {}),
+                      ],
+                    ),
+                  ),
+                ])),
+            Text("私たちの強み", style: GoogleFonts.notoSans(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.black)),
+          ]),
+        ),
       ),
     );
   }
